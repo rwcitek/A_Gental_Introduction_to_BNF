9c25114d0c6d817be8df51cfdb5e99a3bfe79cb4
Ed Howland <ed.howland@gmail.com>
2022-03-01 20:56:35 -0500

    Expanded delta assoc arry to allow : foobar and foobaz, but not any other 6 char string TODO: must get for loop to use $N

--- examples/dfa-engine.sh
+++ examples/dfa-engine.sh
@@ -5,26 +5,33 @@ next() {
 }
 # get length of string
 len() {
-  echo $STR | wc -c
+  echo -n $1 | wc -c
 }
-# delta fn
+# delta fn and final fn. Must be supplied by user
 delta() {
   st=$1; ch=$2
-  declare -A lut=( ["S0,f"]=S1 ["S1,o"]=S2 ["S2,o"]=S3 ["S3,b"]=S4 ["S4,a"]=S5 ["S5,r"]=S6 )
-  echo ${lut["${st},${ch}"]}
+  declare -A lut=( ["S0,f"]=S1 ["S1,o"]=S2 ["S2,o"]=S3 ["S3,b"]=S4 ["S4,a"]=S5 ["S5,r"]=S6 ["S5,z"]=S7 )
+  [ -z ${lut["${st},${ch}"]} ] && echo SErr || echo ${lut["${st},${ch}"]}
+}
+final() {
+  st=$1
+[ $st == S6 ] && return 0
+  [ $st == S7 ] && return 0
+  return 1
 }
 
 
 # store string
-STR="foobar"
+STR=$1         #"foobar"
 state=S0 # Set the start state
 
-N=$(len)
+N=$(len $STR)
 echo N is $N
 for OFF in {1..6}
 do
-  echo state: $state : char $(next)
+#  echo state: $state : char $(next)
   state=$(delta $state $(next))
   OFF=$(echo "$OFF + 1" | bc)
 done
 echo Final state $state
+final $state || exit 1
