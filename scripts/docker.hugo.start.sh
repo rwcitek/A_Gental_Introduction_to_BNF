5ba1b862b40ecc02136863da475516cd2be12d03
Robert Citek <robert.citek@gmail.com>
2022-03-15 11:38:11 -0600

    modify for rwc environment

--- scripts/docker.hugo.start.sh
+++ scripts/docker.hugo.start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd ~/dev/A_Gentle_Intro_to_BNF/hugo/A_Gental_Introduction_to_BNF
-docker run --rm -it -v ${PWD}:/work -p 8080:1313 edhowland/hugo hugo server --bind 0.0.0.0 -D 
-# /bin/bash
-
-#   
+## cd ~/dev/A_Gentle_Intro_to_BNF/hugo/A_Gental_Introduction_to_BNF
+cd ./hugo/A_Gental_Introduction_to_BNF &&
+docker run -d -v ${PWD}:/work -p 8080:1313 edhowland/hugo hugo server --bind 0.0.0.0 -D 
 
