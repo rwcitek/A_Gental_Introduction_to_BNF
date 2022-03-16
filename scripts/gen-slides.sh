42181f6b2fdd369cc45a85842d63ba15a0318d2f
Robert Citek <robert.citek@gmail.com>
2022-03-15 23:59:24 -0600

    in ./scripts/gen-slides.sh: refactored to remove only one file instead of entire folder

--- scripts/gen-slides.sh
+++ scripts/gen-slides.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run this to re-generate new slide deck
 ## cat ~/dev/A_Gentle_Intro_to_BNF/slides/???_*.md > ~/dev/A_Gentle_Intro_to_BNF/hugo/A_Gental_Introduction_to_BNF/content/_index.md 
-rm -rf ./hugo/A_Gental_Introduction_to_BNF/
 mkdir -p ./hugo/A_Gental_Introduction_to_BNF/content/
-cat ./slides/???_*.md > ./hugo/A_Gental_Introduction_to_BNF/content/_index.md 
+rm -rf ./hugo/A_Gental_Introduction_to_BNF/content/_index.md
+cat ./slides/???_*.md > ./hugo/A_Gental_Introduction_to_BNF/content/_index.md
